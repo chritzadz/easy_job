7937e30f0a11e5b6db3dbed612894b8148a5683a
Christian Anthony Dumanauw <chrisdumanauw@gmail.com>
2025-04-24 18:03:22 -0300

    wrong commit

--- app/src/main/java/com/example/template/view/JobActivity.java
+++ app/src/main/java/com/example/template/view/JobActivity.java
@@ -1,6 +1,23 @@
 package com.example.template.view;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.template.Firebase.FirebaseUseCase;
+import com.example.template.R;
+
 public class JobActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_job);
+
+
+        setContents();
+        setEventListeners();
+
+        FirebaseUseCase.set(this);
+    }
 }
