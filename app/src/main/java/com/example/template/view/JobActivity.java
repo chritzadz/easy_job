488c7855744e274a4f465584706a6808d9a6f5fd
Christian Anthony Dumanauw <chrisdumanauw@gmail.com>
2025-04-27 14:54:49 -0300

    set up search layout

--- app/src/main/java/com/example/template/view/JobActivity.java
+++ app/src/main/java/com/example/template/view/JobActivity.java
@@ -1,13 +1,28 @@
 package com.example.template.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.template.Adapter.JobAdapter;
 import com.example.template.Firebase.FirebaseUseCase;
 import com.example.template.R;
+import com.example.template.model.CurrentUser;
+import com.example.template.model.Job;
+import com.example.template.model.User;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class JobActivity extends AppCompatActivity {
+import java.util.List;
+
+public class JobActivity extends AppCompatActivity implements JobAdapter.JobClickListener{
+    User currUser = CurrentUser.getInstance().getUser();
+    TextView searchInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +34,63 @@ public class JobActivity extends AppCompatActivity {
         setEventListeners();
 
         FirebaseUseCase.set(this);
+    }
+
+    private void setEventListeners() {
+    }
+
+    private void setContents() {
+        searchInput = findViewById(R.id.searchTextViewJob);
+
+        RecyclerView resultView = findViewById(R.id.jobListRecyclerViewJob);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        resultView.setLayoutManager(manager);
+
+        DividerItemDecoration decoration = new DividerItemDecoration(resultView.getContext(), manager.getOrientation());
+        resultView.addItemDecoration(decoration);
+
+        JobAdapter adapter = new JobAdapter(FirebaseUseCase.getJobsFromOthers(currUser.getEmail()));
+        adapter.setJobClickListener(this);
+        resultView.setAdapter(adapter);
+
+        showNavigation();
+    }
+
+    private void showNavigation() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setPadding(0,0,0,0);
+        bottomNavigationView.setSelectedItemId(R.id.nav_job);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_settings) {
+                move2ProfilePage();
+                return true;
+            } else if (item.getItemId() == R.id.nav_home) {
+                move2HomePage();
+                return true;
+            } else if (item.getItemId() == R.id.nav_job) {
+                return true;
+            }
+            return false;
+        });
+    }
+
+    @Override
+    public void onJobClick(View view, int position) {
+        Intent intent = new Intent(this, JobDetailsActivity.class);
+
+        List<Job> jobList = FirebaseUseCase.getJobsFromOthers(currUser.getEmail());
+
+        intent.putExtra("clickedJob", jobList.get(position));
+        startActivity(intent);
+    }
+
+    private void move2HomePage() {
+        Intent intent = new Intent(this, DashboardActivity.class);
+        startActivity(intent);
+    }
+
+    private void move2ProfilePage() {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
